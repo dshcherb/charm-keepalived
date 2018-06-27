5965cbb86131fd4d8f121e2700a9c5826700691e
Dmitrii Shcherbakov <dmitrii.shcherbakov@canonical.com>
2018-06-27 16:55:43 +0200

    replace deprecated state function calls

--- src/reactive/keepalived.py
+++ src/reactive/keepalived.py
@@ -2,8 +2,8 @@ import os
 import re
 from subprocess import check_output
 
-from charms.reactive import set_state, when, when_not
-from charms.reactive.flags import remove_state
+from charms.reactive import set_flag, when, when_not, hook
+from charms.reactive.flags import clear_flag
 
 
 from charmhelpers.core.templating import render
@@ -26,7 +26,7 @@ def install_keepalived_package():
     apt_update(fatal=True)
     apt_install('keepalived', fatal=True)
 
-    set_state('keepalived.package.installed')
+    set_flag('keepalived.package.installed')
 
 def default_route_interface():
     ''' Returns the network interface of the system's default route '''
@@ -72,12 +72,12 @@ def configure_keepalived_service():
     service_restart('procps')
 
     status_set('active', 'VIP ready')
-    set_state('keepalived.started')
+    set_flag('keepalived.started')
 
 
 @when('config.changed')
 def reconfigure():
-    remove_state('keepalived.started')
+    clear_flag('keepalived.started')
 
 
 @when('website.available', 'keepalived.started')
